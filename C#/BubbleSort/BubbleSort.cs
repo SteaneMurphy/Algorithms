427c0a98aa9cc7ff3d96118268fccb8823df8ae1
Steane <james@steanemurphy.com>
2024-07-10 16:06:50 +1000

    BubbleSort algorithm/tests added (C#)

---
+++ C#/BubbleSort/BubbleSort.cs
@@ -0,0 +1,122 @@
+﻿namespace Algorithms
+{
+    public class BubbleSort
+    {
+        //sort given array in ascending order
+        public (int[], int) SortAscending(int[] array) 
+        {
+            bool isSorted = false;
+            int counter = 0;
+
+            //iterate over list until sorted
+            while (!isSorted) 
+            {
+                counter++;
+                //check if each element is lower/higher than element to its right
+                //swap if comparison met
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+                
+                //check if list is ordered
+                for (int i = 0; i < array.Length - 1; i++) 
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        isSorted = false;
+                        break;
+                    }
+                    else 
+                    {
+                        isSorted = true;
+                    }
+                }
+            }
+            return (array, counter);
+        }
+
+        //sort given array in descending order
+        public (int[], int) SortDescending(int[] array)
+        {
+            bool isSorted = false;
+            int counter = 0;
+
+            //iterate over list until sorted
+            while (!isSorted)
+            {
+                counter++;
+                //check if each element is lower/higher than element to its right
+                //swap if comparison met
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        int temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+
+                //check if list is ordered
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        isSorted = false;
+                        break;
+                    }
+                    else
+                    {
+                        isSorted = true;
+                    }
+                }
+            }
+            return (array, counter);
+        }
+
+        public (int[], int) SortRecursion(int[] _array, int _counter) 
+        {
+            int counter = _counter;
+            int[] array = _array;
+            bool isSorted = false;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    int temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                }
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    isSorted = false;
+                    break;
+                }
+                else
+                {
+                    isSorted = true;
+                }
+            }
+
+            if (isSorted)
+            {
+                return (array, counter);
+            }
+            else 
+            {
+                return SortRecursion(array, counter + 1);
+            }
+        }
+    }
+}
---
+++ C#/BubbleSort/Main.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Algorithms;
+using System.Diagnostics;
+
+/*******************************************************************************
+ *                               BUBBLE SORT                                   *
+ *                               SOLUTION BY:                                  *
+ *                                 STEANE                                      *
+ *******************************************************************************/
+
+// This sorting algorithm loops through each element in a data set. Each element is compared to the
+// next element and switched based on whether it is ascending or descending output.
+// I tried using recursion but that causes a stackoverflow error for this algorithm due to limited stack
+// space. On average, each recursion call uses 1000KB of space, this test uses 50k iterations and approximately
+// 50MB of stack space, which is over the 1MB limit set by Windows.
+
+// A while loop is more efficient and quicker, it also does not have stackoverflow issues. Because of this
+// the while loop structure was chosen to build the BubbleSort algorithm
+
+// The tests below generate a random integer set of 50k numbers and sort both ascending and descending, outputting the
+// sorted lists and their respective time taken.
+
+namespace Main
+{
+    internal class Entry
+    {
+        static void Main(string[] args)
+        {
+            //create new sort instance and timers
+            BubbleSort newSort = new BubbleSort();
+            Stopwatch timer = new Stopwatch();
+            Stopwatch timer2 = new Stopwatch();
+
+            //create randomised 50k list of integers
+            int[] unsortedArray = RandomSet();
+
+            //test 1
+            timer.Start();
+            (int[] sortedArray, int counter) = newSort.SortAscending(unsortedArray);
+            timer.Stop();
+
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.WriteLine(sortedArray[i]);
+            }
+
+            //test 2
+            timer2.Start();
+            (int[] sortedArray2, int counter2) = newSort.SortDescending(unsortedArray);
+            timer2.Stop();
+
+            for (int i = 0; i < sortedArray2.Length; i++)
+            {
+                Console.WriteLine(sortedArray2[i]);
+            }
+
+            //output messaging and timings
+            Console.WriteLine("\nBUBBLE SORT (ASCENDING ORDER)");
+            Console.WriteLine($"   -> Loop iterations: {counter}");
+            Console.WriteLine($"   -> Time taken: {timer.Elapsed.ToString(@"m\:ss\.fff")}ms\n");
+
+            Console.WriteLine("\nBUBBLE SORT (DESCENDING ORDER)");
+            Console.WriteLine($"   -> Loop iterations: {counter2}");
+            Console.WriteLine($"   -> Time taken: {timer2.Elapsed.ToString(@"m\:ss\.fff")}ms\n");
+        }
+
+        //create random set of 50k numbers to test
+        static int[] RandomSet()
+        {
+            Random rnd = new Random();
+            int[] array = new int[50000];
+
+            for (int i = 0; i < 50000; i++) 
+            {
+                array[i] = rnd.Next();
+            }
+
+            return array;
+        }
+    }
+}
