3ce420bc580904783a77087052dd59919f8b654d
Steane <james@steanemurphy.com>
2024-07-10 20:23:47 +1000

    BubbleSort: improved algorithm, further tests

--- C#/BubbleSort/BubbleSort.cs
+++ C#/BubbleSort/BubbleSort.cs
@@ -1,4 +1,6 @@
-﻿namespace Algorithms
+﻿using System.Diagnostics;
+
+namespace Algorithms
 {
     public class BubbleSort
     {
@@ -12,29 +14,17 @@
             while (!isSorted) 
             {
                 counter++;
+                isSorted = true;
                 //check if each element is lower/higher than element to its right
                 //swap if comparison met
-                for (int i = 0; i < array.Length - 1; i++)
+                for (int i = 0; i < (array.Length - 1) - (counter - 1); i++)
                 {
                     if (array[i] > array[i + 1])
                     {
                         int temp = array[i + 1];
                         array[i + 1] = array[i];
                         array[i] = temp;
-                    }
-                }
-                
-                //check if list is ordered
-                for (int i = 0; i < array.Length - 1; i++) 
-                {
-                    if (array[i] > array[i + 1])
-                    {
                         isSorted = false;
-                        break;
-                    }
-                    else 
-                    {
-                        isSorted = true;
                     }
                 }
             }
@@ -51,72 +41,21 @@
             while (!isSorted)
             {
                 counter++;
+                isSorted = true;
                 //check if each element is lower/higher than element to its right
                 //swap if comparison met
-                for (int i = 0; i < array.Length - 1; i++)
+                for (int i = 0; i < (array.Length - 1) - (counter - 1); i++)
                 {
                     if (array[i] < array[i + 1])
                     {
                         int temp = array[i + 1];
                         array[i + 1] = array[i];
                         array[i] = temp;
-                    }
-                }
-
-                //check if list is ordered
-                for (int i = 0; i < array.Length - 1; i++)
-                {
-                    if (array[i] < array[i + 1])
-                    {
                         isSorted = false;
-                        break;
-                    }
-                    else
-                    {
-                        isSorted = true;
                     }
                 }
             }
             return (array, counter);
-        }
-
-        public (int[], int) SortRecursion(int[] _array, int _counter) 
-        {
-            int counter = _counter;
-            int[] array = _array;
-            bool isSorted = false;
-
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                if (array[i] > array[i + 1])
-                {
-                    int temp = array[i + 1];
-                    array[i + 1] = array[i];
-                    array[i] = temp;
-                }
-            }
-
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                if (array[i] > array[i + 1])
-                {
-                    isSorted = false;
-                    break;
-                }
-                else
-                {
-                    isSorted = true;
-                }
-            }
-
-            if (isSorted)
-            {
-                return (array, counter);
-            }
-            else 
-            {
-                return SortRecursion(array, counter + 1);
-            }
         }
     }
 }
--- C#/BubbleSort/Main.cs
+++ C#/BubbleSort/Main.cs
@@ -32,36 +32,46 @@ namespace Main
             Stopwatch timer2 = new Stopwatch();
 
             //create randomised 50k list of integers
+            //have to make seperate copies of the original array as it is referenced
             int[] unsortedArray = RandomSet();
+            int[] unsortedCopy1 = new int[unsortedArray.Length];
+            int[] unsortedCopy2 = new int[unsortedArray.Length];
+            Array.Copy(unsortedArray, unsortedCopy1, unsortedArray.Length);
+            Array.Copy(unsortedArray, unsortedCopy2, unsortedArray.Length);
 
-            //test 1
+
+            ////test 1
             timer.Start();
-            (int[] sortedArray, int counter) = newSort.SortAscending(unsortedArray);
+            (int[] sortedArray, int counter) = newSort.SortAscending(unsortedCopy1);
             timer.Stop();
 
             for (int i = 0; i < sortedArray.Length; i++)
             {
                 Console.WriteLine(sortedArray[i]);
             }
+            bool test = CheckSortedAsc(sortedArray);
 
             //test 2
             timer2.Start();
-            (int[] sortedArray2, int counter2) = newSort.SortDescending(unsortedArray);
+            (int[] sortedArray2, int counter2) = newSort.SortDescending(unsortedCopy2);
             timer2.Stop();
 
-            for (int i = 0; i < sortedArray2.Length; i++)
+            for (int j = 0; j < sortedArray2.Length; j++)
             {
-                Console.WriteLine(sortedArray2[i]);
+                Console.WriteLine(sortedArray2[j]);
             }
+            bool test2 = CheckSortedDesc(sortedArray2);
 
             //output messaging and timings
             Console.WriteLine("\nBUBBLE SORT (ASCENDING ORDER)");
             Console.WriteLine($"   -> Loop iterations: {counter}");
             Console.WriteLine($"   -> Time taken: {timer.Elapsed.ToString(@"m\:ss\.fff")}ms\n");
+            Console.WriteLine($"   -> List order checked: {test}");
 
             Console.WriteLine("\nBUBBLE SORT (DESCENDING ORDER)");
             Console.WriteLine($"   -> Loop iterations: {counter2}");
             Console.WriteLine($"   -> Time taken: {timer2.Elapsed.ToString(@"m\:ss\.fff")}ms\n");
+            Console.WriteLine($"   -> List order checked: {test2}");
         }
 
         //create random set of 50k numbers to test
@@ -70,12 +80,38 @@ namespace Main
             Random rnd = new Random();
             int[] array = new int[50000];
 
-            for (int i = 0; i < 50000; i++) 
+            for (int i = 0; i < array.Length; i++) 
             {
                 array[i] = rnd.Next();
             }
 
             return array;
+        }
+
+        //checks list is ordered ascending
+        static bool CheckSortedAsc(int[] array) 
+        {
+            for (int i = 0; i < array.Length - 1; i++) 
+            {
+                if (array[i] > array[i + 1]) 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //checks list is ordered descending
+        static bool CheckSortedDesc(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
